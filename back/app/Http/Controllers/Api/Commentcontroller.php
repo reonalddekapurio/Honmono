9a162c140559975a60c030276f15bc867f54d58d
野良猫 <ruirudo@icloud.com>
2025-12-30 18:53:51 +0900

    fix: Commentマイグレーション修正されてなかったのでここで修正

---
+++ back/app/Http/Controllers/Api/Commentcontroller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Commentcontroller extends Controller
+{
+    // コメント一覧取得
+    public function index() {
+        //
+    }
+
+    // ニュースにコメントを返信
+    public function store(Request $request) {
+        
+    }
+}
