11f8e83bff8c353e6cc53db2aa9e6cd32e4f40f4
junpei-chan <jumpei24s1102270@gmail.com>
2026-01-25 02:30:44 +0900

    [update] TailwindCSSの記法を変更

--- front/src/components/layouts/NewspaperCard.tsx
+++ front/src/components/layouts/NewspaperCard.tsx
@@ -19,7 +19,7 @@ export function NewspaperCard({ newspaper, user }: NewspaperCardProps) {
         : newspaper.description;
 
     return (
-        <div className="relative flex flex-col align-start justify-center w-[353px] h-[333px] gap-4 bg-[url('/img/news-paper/news-paper-type1.svg')] bg-no-repeat bg-center bg-cover shadow-2xl p-4">
+        <div className="relative flex flex-col align-start justify-center w-88.25 h-83.25 gap-4 bg-[url('/img/news-paper/news-paper-type1.svg')] bg-no-repeat bg-center bg-cover shadow-2xl p-4">
             <div className="flex align-start justify-center w-full overflow-hidden">
                 <Image 
                 src={newspaper.images} 
@@ -29,7 +29,7 @@ export function NewspaperCard({ newspaper, user }: NewspaperCardProps) {
                 className="w-full h-full object-cover"
             />
             </div>
-            <div className="w-[300px]">
+            <div className="w-75">
                 <p className="text-black text-md font-bold">{displayTitle}</p>
                 <p className="text-gray text-sm font-md">{displayDescription}</p>
                 {isLongDescription && (
