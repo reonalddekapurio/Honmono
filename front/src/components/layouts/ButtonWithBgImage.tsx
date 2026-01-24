b037ddc98240107b0eece38dc494ac51cca8a8d9
junpei-chan <jumpei24s1102270@gmail.com>
2026-01-25 00:51:56 +0900

    [add] ButtonWithBgImageコンポーネントの作成

---
+++ front/src/components/layouts/ButtonWithBgImage.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+
+interface Props {
+  children: string; // 子要素はテキストのみを受け取るのでstring
+  onClick?: () => void;
+}
+
+export function ButtonWithBgImage({
+  children,
+  onClick
+}: Props){
+  return (
+    <button
+      onClick={onClick}
+      className="relative w-36.75 h-7.75 text-white text-base font-yosugara"
+    >
+      <Image 
+        src="/img/tag/tag-red.svg"
+        alt=""
+        fill
+        className="absolute object-cover -z-50"
+        priority
+      />
+      {children}
+    </button>
+  )
+}
