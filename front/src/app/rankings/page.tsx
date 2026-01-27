052dce3318cc79d61bb60cd3a474a2d14b4bd08e
junpei-chan <jumpei24s1102270@gmail.com>
2026-01-28 04:04:52 +0900

    [add] ランキングページのUI実装

--- front/src/app/rankings/page.tsx
+++ front/src/app/rankings/page.tsx
@@ -1,5 +1,39 @@
+import Image from "next/image";
+import rankingsMockData from "@/mocks/rankings.json";
+import { NewspaperCard } from "@/components/layouts";
+
 export default function Rankings() {
   return (
-    <main></main>
+    <main>
+      <div className="flex flex-col items-center gap-6">
+        {rankingsMockData.map((data, i) => {
+          const index = i + 1;
+          
+          return (
+            <div
+              key={data.id}
+              className="flex flex-col items-center gap-6"
+            >
+              <div className="relative w-36 h-11 flex justify-center items-center">
+                <Image
+                  src={`/img/tag/tag-ranking${index <= 3 ? index : "-4below"}.svg`}
+                  alt={`${index}位`}
+                  fill
+                  className="absolute"
+                />
+                <span className="relative z-10 text-black text-xl font-yosugara">
+                  {index}位
+                </span>
+              </div>
+              <NewspaperCard
+                key={data.id}
+                newspaper={data.newspaper}
+                user={data.user}
+              />
+            </div>
+          )
+        })}
+      </div>
+    </main>
   )
 }
